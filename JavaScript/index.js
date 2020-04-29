93b4436027180292528af1b467dc8dd919bf08c7
BuleandraGeorge <buleandrageorge@gmail.com>
2020-04-29 13:18:04 +0000

    MinMax Algorithm

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -104,7 +104,7 @@ function winnerCheck()
                 { line="second";
                   return table[0][2];
                 }
-        if(moveCounter==9) //if max number of moves is reached return null;
+        if(moveCounter==9) //if max number of moves is reached return 0;
             return 0;
     return undefined;
 }; 
@@ -133,7 +133,7 @@ function wonLine(line,coordonate)
         break;
     }
 };
-let winningCase=winnerCheck();
+
 function gameOver(winningCase){
     switch (winnerCheck(winningCase,coordonate,line))
         {
@@ -165,7 +165,7 @@ function minmax(table,moveCounter,isComputer)
 {   
     let winningCase=winnerCheck();
     let score=mmGameOver(winningCase);
-    if (((score==10)||(score==-10)||(score==0))&&(moveCounter<10)){
+    if ((score==10)||(score==-10)||(score==0)){
         return score;
     }
     if(isComputer==false) //minimizer
@@ -175,24 +175,25 @@ function minmax(table,moveCounter,isComputer)
                 {if (table[n][m]==null) //test if the slot is empty
                     { 
                         table[n][m]="X";
-                        console.log(`mini${moveCounter}`);
-                        score=minmax(table,moveCounter+1,true);
-                        bestScore=Math.min(score,bestScore);
+                        let score=minmax(table,moveCounter+1,true);
+                        bestScore=Math.min(score, bestScore);
                         table[n][m]=null;
                      }
                 }
             }
+        console.log(`mini ${bestScore}`);
         return bestScore;
-     }else if (isComputer==true){
-         let bestScore=-Infinity;
+     }else if (isComputer==true)
+     {
+        let bestScore=-Infinity;
         for ( let n=0;n<3;n++) //this for goes through all of the rows
             {for( let m=0;m<3;m++)// this goes through all the columns
                 {if (table[n][m]==null) //test if the slot is empty
                     { 
                         table[n][m]="O";
-                        score=minmax(table,moveCounter+1,false);
-                        table[n][m]=null;
+                        let score=minmax(table,moveCounter+1,false);
                         bestScore=Math.max(score,bestScore);
+                        table[n][m]=null;
                      }
                 }
             }
@@ -221,7 +222,6 @@ tableElem.addEventListener('click',function(event){
                         let currentCellId=move.id;
                         table[currentCellId[0]][currentCellId[1]]=currentPlayer;
                         moveCounter++;
-                        console.log(`Player ${moveCounter}`);
                         pSwitch();
                         if(moveCounter>4){
                             let valueWinner=winnerCheck();
@@ -229,7 +229,6 @@ tableElem.addEventListener('click',function(event){
                         }
                         computerTurn=true;
                     }else { 
-                        console.log(`${computerTurn}`)
                         gsStatus();
                      }
  if ((computerTurn==true)&&(computerStatus==true)&&(gameStatus==1)) 
@@ -248,7 +247,6 @@ function computer()
        for(let m=0;m<3;m++)// this goes through all the columns
             {if (table[n][m]==null){//test if the slot is empty
                     table[n][m]="O";
-                    console.log(`Computer move${moveCounter}`); 
                     let score=minmax(table,moveCounter+1,false);
                     table[n][m]=null;
                     if (score>bestScore){
@@ -262,13 +260,14 @@ function computer()
     table[bestRow][bestColumn]="O";
     document.getElementById(`${bestRow}${bestColumn}`).innerHTML='O';
     moveCounter++;
+    computerTurn=false;
     pSwitch()
     if (moveCounter>4) //test if are enough moves to check if there is a winner;
         {
           let valueWinner=winnerCheck();
           gameOver(valueWinner);
          }
-    computerTurn=false;
+   
 }; 
 //Theme Changer 
 themeMenu.addEventListener("click",function(themeEvent){
